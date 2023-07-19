190e6ba212669c6fd7b5b3f22dcc362d5b1fbeb5
Victor Gonzalez (Support Engineer) <victorgonzalez@salesforce.com>
2023-07-19 16:42:22 +0100

    Initial commit

---
+++ force-app/main/default/lwc/firstLwc/firstLwc.js
@@ -0,0 +1,9 @@
+import { LightningElement } from 'lwc';
+
+export default class FirstLwc extends LightningElement {
+    name = 'Victor';
+    company = "B2B tech";
+    designation = 'Founder';
+    salary = '$100000';
+
+}
